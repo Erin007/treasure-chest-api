8cc8b63b09f64d5c08f6901c567796ee55f607a8
Erin Bond <erin@erinebond.com>
2017-01-03 15:28:18 -0800

    Commented out submission validations

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,8 +1,8 @@
 class Submission < ApplicationRecord
-  belongs_to :directive
-  belongs_to :team 
-
-  validates :directive_id, presence: true
-  validates :team_id, presence: true
-  validates :status, numericality: true {:less_than_or_equal_to 3}
+  # belongs_to :directive
+  # belongs_to :team
+  #
+  # validates :directive_id, presence: true
+  # validates :team_id, presence: true
+  # validates :status, numericality: true {:less_than_or_equal_to 3}
 end

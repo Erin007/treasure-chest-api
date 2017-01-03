e4cac447dc96e54700e19f3af8a4906bc31e62ff
Erin Bond <erin@erinebond.com>
2017-01-03 14:40:35 -0800

    Added description to hunts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103185656) do
+ActiveRecord::Schema.define(version: 20170103223955) do
 
   create_table "directives", force: :cascade do |t|
     t.string   "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170103185656) do
     t.string   "passcode"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.text     "description"
   end
 
   create_table "organizers", force: :cascade do |t|

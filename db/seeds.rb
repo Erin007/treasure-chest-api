5225244ab280eaa726c3a42298d910017ee69c66
Erin Bond <erin@erinebond.com>
2017-01-03 15:48:49 -0800

    Fixed commas in the hunts seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,14 +30,14 @@ Directive.create([
 Hunt.create([
   {
     name: 'Ada First Week Team Building',
-    description: 'Explore Seattle and bond with your new classmates at Ada Developers Academy'
+    description: 'Explore Seattle and bond with your new classmates at Ada Developers Academy',
     organizer_id: 1,
     passcode: 'AdaLovelaceCodesIt'
   },
   {
-    name: 'Mr. Mulligan Fibonacci Finders'
-    description: 'How many examples of the Fibonacci series can you find and submit to discuss in class?'
-    organizer_id: 2
+    name: 'Mr. Mulligan Fibonacci Finders',
+    description: 'How many examples of the Fibonacci series can you find and submit to discuss in class?',
+    organizer_id: 2,
     passcode: 'MulliganMathematicians'
   }
   ])

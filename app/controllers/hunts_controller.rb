eaacc7e682f8c6aa2caa326ec26010e3dbedd8bb
Erin Bond <erin@erinebond.com>
2017-01-03 15:40:44 -0800

    Hunt controller methods mimic directives controller

--- app/controllers/hunts_controller.rb
+++ app/controllers/hunts_controller.rb
@@ -1,26 +1,66 @@
 class HuntsController < ApplicationController
   def index
+    render json: {
+      count: Hunt.count,
+      hunts: Hunt.all.as_json(only: [:id, :name, :description, :organizer_id, :passcode])
+    }
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def show
+    begin
+      render json: hunt.as_json(only: [:id, :name, :description, :organizer_id, :passcode])
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
   end
 
   def update
+    begin
+      hunt.assign_attributes(hunt_params)
+
+      if hunt.save
+        render status: :no_content, content: false
+      else
+        render status: :bad_request, json: {
+          errors: hunt.errors.messages
+        }
+      end
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
   end
 
   def destroy
+    begin
+      hunt.destroy
+      render status: :no_content, content: false
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
   def create
+    hunt = Hunt.new(hunt_params)
+    if hunt.save
+      render status: :created, json: {id: hunt.id}
+    else
+      render status: :bad_request, json: {
+        errors: hunt.errors.messages
+      }
+    end
   end
 
 private
+  def hunt
+   @hunt ||= Hunt.find(params[:id].to_i)
+  end
+
   def hunt_params
     params.require(:hunt).permit(:decription, :passcode, :name, :organizer_id)
   end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,7 +1,7 @@
 class Team < ApplicationRecord
- has_many :players, through: :team_players
- has_many :submissions
-
- validates :name, presence: true
- validates :points, numericality: true
+ # has_many :players, through: :team_players
+ # has_many :submissions
+ #
+ # validates :name, presence: true
+ # validates :points, numericality: true
 end

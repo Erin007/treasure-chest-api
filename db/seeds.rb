5599434d922cdb6e2787866fd0732e8602aa1e22
Erin Bond <erin@erinebond.com>
2017-01-03 15:45:18 -0800

    Seeded 2 hard-coded hunts

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,10 +27,17 @@ Directive.create([
   }
 ])
 
-# Hunt.create!([
-#   {
-#     name: 'Ada First Week Team Building',
-#     organizer_id: 1,
-#     passcode: 'AdaLovelaceCodesIt'
-#   }
-#   ])
+Hunt.create([
+  {
+    name: 'Ada First Week Team Building',
+    description: 'Explore Seattle and bond with your new classmates at Ada Developers Academy'
+    organizer_id: 1,
+    passcode: 'AdaLovelaceCodesIt'
+  },
+  {
+    name: 'Mr. Mulligan Fibonacci Finders'
+    description: 'How many examples of the Fibonacci series can you find and submit to discuss in class?'
+    organizer_id: 2
+    passcode: 'MulliganMathematicians'
+  }
+  ])

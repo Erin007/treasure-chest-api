767c7c6641d30736aee4017df392327209bf25c1
Erin Bond <erin@erinebond.com>
2017-01-03 13:56:27 -0800

    Methods for directives controller based on tasklist-api

--- app/controllers/directives_controller.rb
+++ app/controllers/directives_controller.rb
@@ -1,26 +1,69 @@
 class DirectivesController < ApplicationController
+
   def index
+    #is it better to only show the directives for a specific hunt?
+    render json: {
+      count: Directive.count,
+      directives: Directive.all.as_json(only: [:id, :description, :complete?, :point_value, :name, :hunt_id])
+    }
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def show
+    begin
+      render json: directive.as_json(only: [:id, :description, :complete?, :point_value, :name, :hunt_id])
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
   end
 
   def update
+    begin
+      directive.assign_attributes(directive_params)
+
+      if directive.save
+        render status: :no_content, content: false
+      else
+        render status: :bad_request, json: {
+          errors: directive.errors.messages
+        }
+      end
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
   end
 
   def destroy
+    begin
+      directive.destroy
+      render status: :no_content, content: false
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
   def create
+    directive = Directive.new(directive_params)
+    if directive.save
+      render status: :created, json: {id: directive.id}
+    else
+      render status: :bad_request, json: {
+        errors: directive.errors.messages
+      }
+    end
   end
 
 private
+
+  def directive
+   @directive ||= Directive.find(params[:id].to_i)
+  end
+
   def directive_params
     params.require(:directive).permit(:decription, :complete?, :point_value, :name, :hunt_id)
   end

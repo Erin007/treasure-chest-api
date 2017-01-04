1abbe818dea22460ac2cdfa5968ddc58f7736603
Erin Bond <erin@erinebond.com>
2017-01-04 10:26:28 -0800

    Team controller methods

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,26 +1,66 @@
 class TeamsController < ApplicationController
   def index
+    render json: {
+      count: Team.count,
+      directives: Team.all.as_json(only: [:id, :name, :points])
+    }
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def show
+    begin
+      render json: team.as_json(only: [:id, :name, :points])
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
   end
 
   def update
+    begin
+      team.assign_attributes(team_params)
+
+      if team.save
+        render status: :no_content, content: false
+      else
+        render status: :bad_request, json: {
+          errors: team.errors.messages
+        }
+      end
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
   end
 
   def destroy
+    begin
+      team.destroy
+      render status: :no_content, content: false
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
   def create
+    team = Team.new(team_params)
+    if team.save
+      render status: :created, json: {id: team.id}
+    else
+      render status: :bad_request, json: {
+        errors: team.errors.messages
+      }
+    end
   end
 
 private
+  def team
+    @team ||= Team.find(params[:id].to_i)
+  end
+
   def team_params
     params.require(:team).permit(:name, :points)
   end

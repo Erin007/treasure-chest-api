ca6da818738705fb48d89610fa69f717d8e3e188
Erin Bond <erin@erinebond.com>
2017-01-13 15:32:44 -0800

    Commented submission validation for numericality

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -4,5 +4,5 @@ class Submission < ApplicationRecord
 
   validates :directive_id, presence: true
   validates :team_id, presence: true
-  validates :status, numericality: true {:less_than_or_equal_to 3}
+  #validates :status, numericality: true {:less_than_or_equal_to 3}
 end

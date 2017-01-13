30fcb17cebc4afe9e478d3fc6998486c57c63a66
Erin Bond <erin@erinebond.com>
2017-01-13 11:13:04 -0800

    Changed seed files to reflect change from complete? to complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,432 +10,432 @@ Directive.create([
   {
     name: 'A tour group',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A bookstore',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A book about Seattle',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A novel that at least 3 of you have read at the Central Library',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'The campiest moive you can find at the Central Library',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A pet that is NOT a dog',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A burlesque flyer',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Flyer for a show that happened over a year ago',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Chihuly glass art',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A climbing wall',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: "A farmer's market",
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A fresh donut',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A fruit or vegetable that no one in your group has tried before',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A group of 4+ bikers',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Someone in Seahawks gear',
     description: 'Go Hawks!',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   }, {
     name: 'Someone in Sounders gear',
     description: 'Seattle Sounders - MLS Champs 2016!',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   }, {
     name: 'A fancy mustache',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A mohawk',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A terrible parking job',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'An out-of-state license plate',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'The Ferris Wheel',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A promotional pen from a software company',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A rainbow',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A ferry',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A Henry mural',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A segway of hoverboard',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'The EMP',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A stadium',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A telephone booth',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A theatre (movie or stage)',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A very small coffee',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Air-born fist at Pike Place Market',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'An antiques store',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A Seattle pig',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: "Baby swings (the ones you can't fall out of)",
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Benaroya Hall',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Food served from a cart or a truck',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: "Fran's Chocolates",
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Olympic Sculpture Park',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'The P-I Globe',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Freeway park',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A working pinball or arcade machine',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Ostentatious recycling',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Put some gum on the gum wall',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A Seattle-centric tattoo',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'SAM',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Someone eating seafood',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Someone using a selfie stick',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Someone using a Microsoft & Apple device at the the same time',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'A Duck Boat',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Something magical',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Something old',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Something brand new',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Something that rattles',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Something wet',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Something with a tail',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Something with batteries',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Teatro ZinZanni',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'The original Starbucks',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: "Beecher's Cheese",
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Yoga pants and a briefcase',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },
   {
     name: 'Your entire group in a picture with the Space Needle',
     description: '',
-    complete?: "false",
+    complete: "false",
     point_value: 1,
     hunt_id: 1
   },

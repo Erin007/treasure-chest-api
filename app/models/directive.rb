912da8ed81106ea50927fa36b3b897467b38c4fc
Erin Bond <erin@erinebond.com>
2017-01-16 18:24:43 -0800

    Commented complete validation in directive model because app wasn't handling false well

--- app/models/directive.rb
+++ app/models/directive.rb
@@ -2,7 +2,7 @@ class Directive < ApplicationRecord
   #belongs_to :hunt
   has_many :submissions
 
-  validates :complete, presence: true
+  #validates :complete, presence: true
   validates :hunt_id, presence: true
   validates :name, presence: true
   validates :point_value, numericality: true

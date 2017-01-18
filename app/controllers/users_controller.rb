43402c80ed324f2ab50052ec2a3af5b60ddeabe3
Erin Bond <erin@erinebond.com>
2017-01-18 11:21:04 -0800

    User model and controller methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,69 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
+    render json: {
+      count: User.count,
+      teams: User.all.as_json(only: [:id, :username, :email])
+    }
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def show
+    begin
+      render json: user.as_json(only: [:id, :username, :email])
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      render status: :created, json: {id: user.id, username: team.username, email: user.email }
+    else
+      render status: :bad_request, json: {
+        errors: user.errors.messages
+      }
+    end
   end
 
   def destroy
+    begin
+      user.destroy
+      render status: :no_content, content: false
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
   end
 
   def update
+    begin
+      user.assign_attributes(user_params)
+
+      if user.save
+        render status: :no_content, content: false
+      else
+        render status: :bad_request, json: {
+          errors: user.errors.messages
+        }
+      end
+    rescue ActiveRecord::RecordNotFound
+      render status: :not_found, content: false
+    end
   end
 
-  def edit
+  # def edit
+  # end
+private
+  def user
+    @user||= User.find(params[:id].to_i)
   end
+
+  def user_params
+    params.permit(:username, :email)
+  end
+
 end
